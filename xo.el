a8e013b215201ba3b9366df4e073e457173f76b6
teehemkay <tmk@exidia.com>
2018-12-26 18:57:49 +0100

    Pass count-valley challenge

--- xo.el
+++ xo.el
@@ -1,24 +1,17 @@
-(require 'flycheck)
-(flycheck-define-checker javascript-xo
-  "JavaScript happiness style linter.
+(with-eval-after-load 'flycheck
+  (progn
+    (flycheck-define-checker javascript-xo
+      "JavaScript happiness style linter.
 
 See URL `https://github.com/xojs/xo'."
-  :command
-    ("xo" "--reporter=unix"
-    "--stdin"
-    (eval (concat "--stdin-filename=" (buffer-file-name))))
-  :standard-input t
-  :error-patterns
-    ((warning line-start
-              (file-name) ":" line ":" column ": "
-              (message) " [Warning/" (id (one-or-more (in lower digit "/-"))) "]" line-end)
-     (error line-start
-            (file-name) ":" line ":" column ": "
-            (message) " [Error/" (id (one-or-more (in lower digit "/-"))) "]" line-end)
-     (error line-start
-            (file-name) ":" line ":" column ": "
-            (message) " [Error]" line-end))
-  :modes
-    (js-mode js-jsx-mode js2-mode js2-jsx-mode js3-mode rjsx-mode))
+      :command
+      ("xo" "--reporter=json"
+       "--stdin"
+       (eval (concat "--stdin-filename=" source-original)))
+      :standard-input t
+      :error-parser flycheck-parse-eslint
+      :modes
+      (js-mode js-jsx-mode js2-mode js2-jsx-mode js3-mode rjsx-mode))
 
-(add-to-list 'flycheck-checkers 'javascript-xo)
+    (add-to-list 'flycheck-checkers 'javascript-xo)
+    ))
